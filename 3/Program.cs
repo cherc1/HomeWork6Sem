01a86ceb765824a3f293fc97a9866e51356561b4
Arina Skripova <skripovaarina1@gmail.com>
2023-12-12 12:34:32 +0300

    создала прототип 3 программы

---
+++ 3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите строку: ");
+string? stroka = Console.ReadLine();
+Console.WriteLine($" {stroka}");
+
+int size = stroka.Length;
+int i = 0;
+while(i < size)
+{
+    foreach(char n in stroka)
+    if(n == size - i)
+    {
+        i++;
+    }
+    else 
+    {
+        Console.WriteLine("Строка не полиндром");
+        break;
+    }
+}
+Console.WriteLine("Строка полиндром");
