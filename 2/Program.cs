c01ec0f521894e954b7caf7c20197e40d0c6142e
Arina Skripova <skripovaarina1@gmail.com>
2023-12-12 12:08:26 +0300

    сделала прототип 2 задачи

---
+++ 2/Program.cs
@@ -0,0 +1,11 @@
+﻿string str = "Arina";
+Console.WriteLine(str);
+
+int n = 0;
+for(int i = 0; i < str.Length; i++)
+{
+    //if(элемент строки верхнего регистра)
+    //
+    n++;
+}
+Console.WriteLine($"Количество букв верхнего регистра - {n}");
