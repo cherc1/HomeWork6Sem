3a320fc71597c6902ecc0ed6fbe15164aa77252f
Arina Skripova <skripovaarina1@gmail.com>
2023-12-12 11:47:05 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,9 @@
+﻿char [,] char_array = new char[2,5]{
+{'w', 'o', 'r', 'l', 'd'},
+{'h', 'e', 'l', 'l', 'o'}};
+
+string str = "";
+foreach(char el in char_array)
+  str += el;
+Console.WriteLine(str);
+
